f2a887805572eb8547fd17c8528a8257932d90b9
Ivanchi <zamoramirez.iv@gmail.com>
2019-02-19 18:14:49 -0600

    libreria potD

---
+++ include/digPot.h
@@ -0,0 +1,44 @@
+int k = 1;
+int tPaso = 10;
+void potUP(int valor){
+
+  digitalWrite(CS, HIGH);
+  digitalWrite(INC, HIGH);
+  digitalWrite(UD, HIGH);
+  delay(10);
+  digitalWrite(CS,LOW);
+  for(k=0;k<valor;k++){
+    digitalWrite(INC, HIGH);
+    delay(tPaso);
+    digitalWrite(INC, LOW);
+    delay(10);
+  }
+  digitalWrite(CS, HIGH);
+  digitalWrite(INC, HIGH);
+}
+void potDOWN(int valor){
+  digitalWrite(CS, HIGH);
+  digitalWrite(INC, HIGH);
+  digitalWrite(UD, LOW);
+  delay(10);
+  digitalWrite(CS,LOW);
+  for(k=0;k<valor;k++){
+    digitalWrite(INC, HIGH);
+    delay(tPaso);
+    digitalWrite(INC, LOW);
+    delay(10);
+  }
+  digitalWrite(CS, HIGH);
+  digitalWrite(INC, HIGH);
+}
+
+void potReset(){
+
+    digitalWrite(INC, HIGH);
+    delay(5);
+    digitalWrite(CS, LOW);
+    delay(5);
+    digitalWrite(CS, HIGH);
+    delay(50);
+
+}
